d61dbeba29dd5e966032ba825b8e0b96f244b22b
Dustin Fedie <dlfedie@gmail.com>
2019-09-04 15:45:33 -0500

    adds heroku and heroku db setup. maybe?

---
+++ build/precache-manifest.0b850fc3ad36727c49ee0df0f6c057d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f57d39a041a9db92898d20e4efd1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e842b939ded35543f0",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "1e13b46c50cbfb55f4e0",
+    "url": "/static/js/2.5a5b528b.chunk.js"
+  },
+  {
+    "revision": "26e842b939ded35543f0",
+    "url": "/static/js/main.6b4c33e2.chunk.js"
+  },
+  {
+    "revision": "56de777fdcbceecc9e61",
+    "url": "/static/js/runtime~main.566326f8.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
